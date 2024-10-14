49c003d4b6a1e9a1c05a4f8b8ccb1165ad41052e
Nhat Truong <nhattruong3314@gmail.com>
2024-10-14 10:13:56 +0700

    fix

--- src/sections/Opening.tsx
+++ src/sections/Opening.tsx
@@ -11,7 +11,7 @@ export default function Opening() {
             </div>
             <div className='description-1'>
                 <p>
-                Tháng 8 năm 1945, dưới sự lãnh đạo của Đảng Cộng sản Đông Dương và Mặt trận Việt Minh, nhân dân Việt Nam đã đứng lên giành chính quyền, đánh dấu sự kết thúc ách thống trị của thực dân Pháp và phát xít Nhật, mở ra kỷ nguyên độc lập cho dân tộc.
+                Tháng 8 năm 1945, dưới sự lãnh đạo của Đảng Cộng sản Đông Dương và Mặt trận Việt Minh, nhân dân Việt Nam đã đứng lên giành chính quyền, đánh dấu sự kết thúc ách thống trị của thực dân Pháp và phát xít Nhật. Cách mạng Tháng Tám là một cuộc cách mạng giải phóng dân tộc lần đầu tiên giành thắng lợi ở một nước thuộc địa, đã đột phá một khâu quan trọng trong hệ thống thuộc địa của chủ nghĩa đế quốc, mở đầu thời kỳ suy sụp và tan rã của chủ nghĩa thực dân cũ.
                 </p>
             </div>
         </div>
